7111a4203a0562e885ecbbfbcc458cba03b0667d
Gaurav Singh <gaurav.singh@go-jek.com>
2021-02-07 09:42:12 +0530

    while loop examples

---
+++ tau-intro-to-js/09-while-loops.js
@@ -0,0 +1,21 @@
+let password = "tomorrow"
+let userGuess = "";
+
+// while (userGuess != password) {
+//     userGuess = prompt("Please enter your password")
+// }
+
+// alert("Hurray! you cracked it")
+
+// JS has do while loop as well
+
+let number = 5
+let factorial = 1
+
+do {
+    factorial *= number
+    number--
+
+} while (number > 0)
+
+console.log("Factorial is: " + factorial)
