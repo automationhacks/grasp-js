0568e74cf033292be4a9774f2f362164fc496f27
Gaurav Singh <gaurav.singh@go-jek.com>
2021-02-09 21:13:40 +0530

    switch env based on passed in variables

---
+++ tau-webdriverio/urls.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'qa': 'https://the-internet.herokuapp.com/',
+    'dev': 'https://automationhacks.io',
+    'staging': 'https://www.google.co.in'
+}
--- tau-webdriverio/wdio.conf.js
+++ tau-webdriverio/wdio.conf.js
@@ -1,3 +1,12 @@
+const urls = require('./urls')
+
+const ENV = process.env.ENV
+
+if (!ENV || !['qa', 'dev', 'staging'].includes(ENV)) {
+    console.log('Please provide env in format: ENV=qa|dev|staging')
+    process.exit()
+}
+
 exports.config = {
     //
     // ====================
@@ -96,7 +105,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://the-internet.herokuapp.com/',
+    baseUrl: urls[process.env.ENV],
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
