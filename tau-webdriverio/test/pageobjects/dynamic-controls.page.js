ed3f35fdd64bfd2bc4310b86d9a4bd1c612a4766
Gaurav Singh <gaurav.singh@go-jek.com>
2021-02-09 08:16:22 +0530

    Wait until

--- tau-webdriverio/test/pageobjects/dynamic-controls.page.js
+++ tau-webdriverio/test/pageobjects/dynamic-controls.page.js
@@ -4,9 +4,20 @@ class DynamicControlsPage extends Page {
     get enabledButton() { return $('#input-example button') }
     get inputEnabledTextBox() { return $('#input-example input') }
     get enabledConfirmationText() { return $('#input-example #message').getText() }
+    get removeOrAddButton() { return $('#checkbox-example button') }
 
     clickEnabledButton() {
         this.enabledButton.click()
+    }
+
+    clickRemoveButton() {
+        this.removeOrAddButton.click()
+    }
+
+    waitTillButtonTextChanges() {
+        browser.waitUntil(() => {
+            return this.removeOrAddButton.getText() === 'Add'
+        }, 6000, 'expect button text to change')
     }
 
     waitForButtonToEnable() {
---
+++ tau-webdriverio/test/specs/wait-until.test.js
@@ -0,0 +1,11 @@
+const dynamicControlsPage = require('../pageobjects/dynamic-controls.page')
+
+describe('wait until tests', () => {
+    it('should wait until the text changes', () => {
+        browser.url(`${browser.options.baseUrl}dynamic_controls`)
+        dynamicControlsPage.clickRemoveButton()
+
+        dynamicControlsPage.waitTillButtonTextChanges()
+        expect(dynamicControlsPage.removeOrAddButton.getText()).to.be.equal('Add')
+    })
+})
