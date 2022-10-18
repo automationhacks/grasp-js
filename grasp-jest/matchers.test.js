4480c030b2ebec158be38e447ce59012201d73c0
Gaurav Singh <gauravsingh1@fb.com>
2022-10-18 21:58:09 +0530

    Added matchers examples for truthiness and numbers

--- grasp-jest/matchers.test.js
+++ grasp-jest/matchers.test.js
@@ -15,3 +15,35 @@ test('adding positive nos is not zero', () => {
         }
     }
 })
+
+// Choose appropriate matcher based on the source code should behave 
+test('truthiness matchers for null values', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('truthiness matchers for zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull(); 
+    expect(z).toBeDefined(); 
+    expect(z).not.toBeUndefined(); 
+    expect(z).not.toBeTruthy(); 
+    expect(z).toBeFalsy(); 
+});
+
+test('numbers test: two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are same for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
